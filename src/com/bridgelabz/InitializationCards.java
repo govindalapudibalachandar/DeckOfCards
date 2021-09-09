97695cb8ee6e1a62b84a02a68e6ea32f09479ec8
balachandar <govindalapudibalachandar@gmail.com>
2021-09-09 09:19:27 +0530

    Added:DeckOfCard file is added

---
+++ src/com/bridgelabz/InitializationCards.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class InitializationCards {
+
+	//public static void main(String[] args) {
+		
+		String suit []= { "s" , "D","C","H"};
+		String rank [] = {"2","3","4","5","6","7","8","9","j","Q","k","a" };
+		String [] deck = new String[52];
+
+	}
+
+
